cf670aff272600987ced2edc56ce1555625829f1
kaveenlk97 <kaveen_lak@bitoku.io>
2022-08-17 23:49:50 +0530

    added comments

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,19 @@ use std::io;
 fn main() {
     println!("Guess the Number :)");
 
+    //generate a random number in range 1 - 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is : {secret_number}");
-
+    //loop until user guess the correct numner
     loop {
         println!("Please enter your guess");
 
-        let mut guess = String::new();
+        let mut guess = String::new(); // by default variables are immutable in RUST, initializing MUTABLE variable called guess
 
+        //read user input
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            .read_line(&mut guess) //read_line return Ok and Err
+            .expect("Failed to read line"); //handle if Result is an Err rather than Ok.
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -27,7 +28,7 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("Hooray!, You guessed the number!");
                 break;
             }
         }
